8550d1b6135ed5a41771e0551a8102ed7ae93217
EddieSwift <37484195+EddieSwift@users.noreply.github.com>
2019-02-11 20:15:39 +0200

    Initial Commit

---
+++ 35UITableViewSearchHW/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  35UITableViewSearchHW
+//
+//  Created by Eduard Galchenko on 2/11/19.
+//  Copyright © 2019 Eduard Galchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 35UITableViewSearchHW/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  35UITableViewSearchHW
+//
+//  Created by Eduard Galchenko on 2/11/19.
+//  Copyright © 2019 Eduard Galchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
